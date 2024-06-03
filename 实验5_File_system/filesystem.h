317a83e4c602f6f2e60ca5568cd8df2961937c05
Xie Yuhang <117255169+varinexyh@users.noreply.github.com>
2024-06-03 19:39:27 +0800

    Update filesystem.h

--- 实验5_File_system/filesystem.h
+++ 实验5_File_system/filesystem.h
@@ -1,34 +1,35 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 #define DISK_SIZE 1024 * 1024 //Total disk size: 1MB
-#define BLOCK_SIZE 512
-#define MAX_FILES 128
-#define MAX_FILENAME 16
-#define MAX_PATH 256
+#define BLOCK_SIZE 512 //We divided the visual storage into 512 blocks.
+#define MAX_FILES 128 //Maximum files stored in single directory.
+#define MAX_FILENAME 16 //Maximum file name length(16 characters).
+#define MAX_PATH 256 //Maximum file path length.
 typedef struct 
 {
     char name[MAX_FILENAME];
     int size;
-    int start_block;
-    int is_directory;
+    int start_block; //To record the start block of single file in physical disk storage.
+    int is_directory; //Mark the file whether a directory.
 } FileEntry;
 typedef struct {
     char name[MAX_FILENAME];
-    int file_count;
+    int file_count; //File number in single directory.
     FileEntry files[MAX_FILES];
 } Directory;
 typedef struct 
 {
-    char data[BLOCK_SIZE];
+    char data[BLOCK_SIZE]; //Physical data stored in disk(read and write operations only).
 } Block;
 typedef struct
 {
-    Block blocks[DISK_SIZE / BLOCK_SIZE];
-    Directory root;
+    Block blocks[DISK_SIZE / BLOCK_SIZE]; //Divide the whole storage in to equal blocks.
+    Directory root; //Root directory of file system.
 } FileSystem;
 extern FileSystem fs;
 extern Directory *current_dir;
 extern FileEntry *opened_file;
+//Below are operations within the file system.
 void my_format();
 void my_mkdir(char *name);
 void my_rmdir(char *name);
